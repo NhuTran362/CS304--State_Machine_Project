b8a5cb784bd69edc90777fefc781e9d169cfaf9e
NhuTran362 <71521437+NhuTran362@users.noreply.github.com>
2020-09-25 00:30:14 +0700

    Create myString.h
    Completed -TASK 1

---
+++ myString.h
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<string>
+#include<set>
+#include <vector>
+
+
+using namespace std;
+
+
+// Completed TASK 1 and 2 - REPRESENTING ALPHABET AND STRING
+
+class myString {
+
+private:
+	set<string> alphabet;
+	vector<string> _string;
+
+	int alphabetSize;
+	int _stringLen;
+
+	// this function will break down each characters in a standard string and put it in _string provided the alphabet
+	//e.g. Alphabet = {ab, c}   standar_string = "abcab"   =>  _stringSize = {ab,c,ab}    
+	vector<string> convert_standard_string_to_vector_based_string(set<string> alphabet, string standard_string);
+
+	//this function will check if a vector-based string contain any symbol does not belong to the given alphabet.
+	bool check_alpha_string(set<string> myAlphabet, vector<string> _string);
+
+public:
+
+	myString() : alphabet(set<string> {"EPISILON"}), _string({ "EPSILON" }), alphabetSize(0), _stringLen(0) {};
+	myString(set<string> initAlphabet, vector<string> initString);
+	myString(string initString);
+	myString(set<string> initAlphabet, string initString);
+
+
+	set<string> getAlphabet() const { return alphabet; }
+	vector<string> getString() const { return _string; }
+	int getAlphabetSize() const { return alphabetSize; }
+	int getStringLen() const { return _stringLen; }
+
+	void print_discrete_character() {
+		for (int i = 0; i < _stringLen;i++)
+			cout << _string[i] << " ";
+	}
+	string getCharacterAt(int i) const { return _string[i]; }
+
+	friend ostream&  operator<<(ostream& OutStream, const myString& myObject);
+
+};
