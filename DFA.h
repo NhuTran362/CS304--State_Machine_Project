73e8c3ddc9386efc2c683301b9956fafc3579e47
NhuTran362 <71521437+NhuTran362@users.noreply.github.com>
2020-11-16 02:45:19 +0700

    Update DFA.h
    add a dozen of test for equality function

--- DFA.h
+++ DFA.h
@@ -216,4 +216,19 @@ void test_string_that_does_not_contain_consecutive_1_SUBSET_even_len();
 
 //TASK 20
 bool DFA_Equality(const DFA& A, const DFA& B);
+
+//TASK 21
+
+void test_even_len_EUQAL_strings_of_0s();
+void test_string_end_with_0_EQUAL_string_of_even_number_of_0();
+void test_string_of_three_consecutive_zero_EUQAL_strings_of_110s();
+void test_string_of_len_2_EQUAL_even_len();
+void test_string_of_even_0_and_1_EQUAL_string_of_even_number_of_0();
+void test_strings_of_110s_EQUAL_string_end_with_zero();
+void test_string_end_with_0_EQUAL_string_of_0s();
+void test_string_of_0s_EQUAL_string_of_0s();
+void test_string_of_even_0_and_1_EQUAL_even_len();
+void string_that_does_not_contain_consecutive_1_EQUAL_string_that_does_not_contain_consecutive_1();
+void test_string_that_accept_only_101_EQUAL_string_that_accept_only_101();
+void test_string_of_len_2_EQUAL_Intersection_of_len_2_and_even_len();
 #endif // !DFA_H
