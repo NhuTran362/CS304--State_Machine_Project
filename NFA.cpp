dfe588287f2b8d1b59c5f3e434560112b749ee9f
NhuTran362 <71521437+NhuTran362@users.noreply.github.com>
2020-11-19 09:52:11 +0700

    Update NFA.cpp
    add function definition for TASK 27
    Completed TASK 27 - (Oracle) Write a function that given an NFA, a string, a trace, and a boolean, determines if the trace is a valid execution of the NFA and accepts results in the given boolean.

--- NFA.cpp
+++ NFA.cpp
@@ -47,3 +47,56 @@ void NFA::operator=(const DFA &dfa) {
 	for (auto each : T)
 		transition[each.first] = set<string>{ each.second };
 }
+
+//TASK 27
+config string_to_config(string str) {
+	config rslt;
+	rslt.q = str[0];
+	str.erase(0,2);
+	if (str == "EPSILON")
+		rslt.w = myString(set<string>{"0", "1"}, vector<string> {str});
+	else
+		rslt.w = myString(str);
+	return rslt;
+}
+bool NFA_Oracle(const NFA& myNFA, string str, list<string> trace){
+if (trace.size() == 0)
+		return true;
+	map<Pair, set<string>> T = myNFA.getTransition();
+	auto p1 = trace.begin();
+	map<Pair,set<string>>::iterator p2;
+	config c1, c2;
+	int i = 0;
+	while (p1 != (--trace.end())) {
+		cout << i << endl;
+		i++;
+		c1 = string_to_config(*p1);
+		p1++;
+		c2 = string_to_config(*p1);
+		
+		if (p1 == trace.begin() && (c1.q != myNFA.getStartState() || c1.w != str))	
+			return false;
+		
+		if (c1.w == c2.w) {
+			p2 = T.find(Pair(c1.q, "EPSILON"));
+			if (p2 == T.end() || p2->second.find(c2.q) == p2->second.end())
+				return false;
+		}
+		else
+		{
+			
+			string a = c1.w.getCharacterAt(0);
+			c1.w.pop_front();
+			if (c1.w == c2.w) {
+				p2 = T.find(Pair(c1.q, a));
+				if (p2 == T.end() || p2->second.find(c2.q) == p2->second.end())
+					return false;
+			}
+			else {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
